447b9f484978eef49e3e4819a51b1ca74de84ace
mathys dikmans <mathys.dikmans@adp.com>
2019-11-19 12:43:53 +0100

    bypass proxy on local

--- XUnitTestProjectNetCoreTwo/UnitTest1.cs
+++ XUnitTestProjectNetCoreTwo/UnitTest1.cs
@@ -11,12 +11,14 @@ namespace Tests
     [Fact]
     public async Task Test1()
     {
-      HttpClient httpClient = new HttpClient
+      HttpClientHandler httpClientHandler = new HttpClientHandler
       {
-        BaseAddress = new Uri("http://localhost:5000/")
+        Proxy = new WebProxy { BypassProxyOnLocal = true }
       };
-
+      
+      HttpClient httpClient = new HttpClient(httpClientHandler) { BaseAddress = new Uri("http://localhost:5000/") };
       var response = await httpClient.GetAsync("api/values");
+      var str = await response.Content.ReadAsStringAsync();
       Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
   }
